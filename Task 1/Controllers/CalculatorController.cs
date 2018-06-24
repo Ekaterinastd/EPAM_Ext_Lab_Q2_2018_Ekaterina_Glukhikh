c71190de530a40165091edc2c7118e4a8dde4dea
Ekaterina_Glukhikh <Ekaterina.G2015@mail.ru>
2018-06-24 22:56:47 +0400

    232141

---
+++ Task 1/Controllers/CalculatorController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_1.Controllers
+{
+    public class CalculatorController : Controller
+    {/// <summary>
+    /// Метод, возвращающий сумму чисел
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+        public int Sum(int x, int y)
+        {
+             return x + y;
+        }
+        /// <summary>
+        /// Метод, возвращающий произведение чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Prod(int x, int y)
+        {
+            return x * y;
+        }
+        // GET: Calculator
+        public ActionResult Calculartor(Models.Model1.Calculating calc)
+        {
+            int selectedAction = Convert.ToInt32(Request["action"]);
+
+            //if ((int.TryParse(Convert.ToString(calc.X), out int buf11)) & int.TryParse(Convert.ToString(calc.Y), out int buf22))
+            //    if ((buf11== 0) & (buf22 == 0)) { Task_1.Models.Expansion.log.Add("Value is not integer"); return View(); }
+            if ((int.TryParse(Convert.ToString(calc.X), out int buf1)) && int.TryParse(Convert.ToString(calc.Y), out int buf2))
+            {
+                if ((buf1 > 0) && (buf2 > 0))
+                {
+                        switch (selectedAction)
+                        {
+                            case 0:
+                                {
+                                    int k = Sum(buf1, buf2);
+                                    Task_1.Models.Expansion.log.Add(DateTime.Now.ToString("dd MMMM HH:mm") + " " + "result: " + Convert.ToString(calc.X) + " + " + Convert.ToString(calc.Y) + " = " + Convert.ToString(k));
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    int k = Prod(buf1, buf2);
+                                    Task_1.Models.Expansion.log.Add(DateTime.Now.ToString("dd MMMM HH:mm") + " " + "result: " + Convert.ToString(calc.X) + " * " + Convert.ToString(calc.Y) + " = " + Convert.ToString(k));
+                                    break;
+                                }
+                        }
+                }
+            }
+            
+            return View();
+        }
+        public ActionResult Result(Models.Model1.Calculating calc)
+        {
+            return View();
+        }
+    }
+}
---
+++ Task 1/Models/Model1.cs
@@ -0,0 +1,47 @@
+﻿namespace Task_1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public class Model1 : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Task_1.Models.Model1" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
+        // в файле конфигурации приложения.
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        
+        public class Calculating
+        {
+            public object X { get; set; }
+            public object Y { get; set; }
+                      
+        }
+    }
+
+    public static class Expansion
+    {
+        public static List<string> log = new List<string>();
+        /// <summary>
+        /// Метод расширения, выводящий результат вычислений
+        /// </summary>
+        /// <param name="html">Сообщает компилятору, что определяется метод расширения</param>
+        /// <returns>Возвращает массив вычислений</returns>
+        public static List<string> GetLog(this HtmlHelper html)
+        {                   
+            return log;
+        }
+    }
+}
