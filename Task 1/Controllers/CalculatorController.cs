921d5575980fe2cdc7549d4a4d4002153a63f021
Ekaterina_Glukhikh <Ekaterina.G2015@mail.ru>
2018-07-01 21:42:33 +0400

    corrected

--- Task 1/Controllers/CalculatorController.cs
+++ Task 1/Controllers/CalculatorController.cs
@@ -8,72 +8,50 @@ namespace Task_1.Controllers
 {
     public class CalculatorController : Controller
     {
-        private static string logOut = string.Empty;
         /// <summary>
         /// Метод, возвращающий сумму чисел
         /// </summary>
         /// <param name="x"></param>
         /// <param name="y"></param>
         /// <returns></returns>
-        public int Sum(int x, int y)
+        public int? Sum(string X, string Y)
         {
-             return x + y;
+            if (int.TryParse(X, out int x))
+                if (int.TryParse(Y, out int y))
+                    return x + y;
+            return null;
         }
         /// <summary>
         /// Метод, возвращающий произведение чисел
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
         /// <returns></returns>
-        public int Prod(int x, int y)
+        public int? Prod(string X, string Y)
         {
-            return x * y;
+            if (int.TryParse(X, out int x))
+                if (int.TryParse(Y, out int y))
+                    return x * y;
+            return null;
         }
         // GET: Calculator
-        public ActionResult Calculartor(Models.Model1.Calculating calc)//todo pn что-то ты намудрила. Нужно 2 метода-экшена, которые возвращают вьюшку одну и ту же, но с разными значениями параметров модели.
+        public ActionResult Calculartor(string X, string Y, string action)//todo pn что-то ты намудрила. Нужно 2 метода-экшена, которые возвращают вьюшку одну и ту же, но с разными значениями параметров модели.
         {
-            int selectedAction = Convert.ToInt32(Request["action"]);
-
-            //if ((int.TryParse(Convert.ToString(calc.X), out int buf11)) & int.TryParse(Convert.ToString(calc.Y), out int buf22))
-            //    if ((buf11== 0) & (buf22 == 0)) { Task_1.Models.Expansion.log.Add("Value is not integer"); return View(); }
-            if ((int.TryParse(Convert.ToString(calc.X), out int buf1)) && int.TryParse(Convert.ToString(calc.Y), out int buf2))
+            int? result = null;
+            switch (action)
             {
-                if ((buf1 > 0) && (buf2 > 0))
-                {
-                        switch (selectedAction)
-                        {
-                            case 0:
-                                {
-                                    int k = Sum(buf1, buf2);
-                                    Task_1.Models.Expansion.log.Add(DateTime.Now.ToString("dd MMMM HH:mm") + " " + "result: " + Convert.ToString(calc.X) + " + " + Convert.ToString(calc.Y) + " = " + Convert.ToString(k));
-                                    break;
-                                }
-                            case 1:
-                                {
-                                    int k = Prod(buf1, buf2);
-                                    Task_1.Models.Expansion.log.Add(DateTime.Now.ToString("dd MMMM HH:mm") + " " + "result: " + Convert.ToString(calc.X) + " * " + Convert.ToString(calc.Y) + " = " + Convert.ToString(k));
-                                    break;
-                                }
-                        }
-                }
+                case "+":
+                    result = Sum(X, Y);
+                    break;
+                case "*":
+                    result = Prod(X, Y);
+                    break;
+                default:
+                    break;
             }
-            
+            if (result != null)
+                Models.Expansion.log.Add(String.Format("{0}: {1} {2} {3} = {4}", DateTime.Now.ToString("dd MMMM HH:mm"), X, action, Y, result));
             return View();
-        }
-        public ActionResult Result(Models.Model1.Calculating calc)
-        {
-            return View();
-        }
-        public static string LogOut
-        {
-            get
-            {
-                return logOut;
-            }
-            set
-            {
-                logOut = value;
-            }
         }
     }
 }
--- Task 1/Models/Model1.cs
+++ Task 1/Models/Model1.cs
@@ -7,31 +7,6 @@
     using System.Linq;
     using System.Web.Mvc;
 
-    public class Model1 : DbContext
-    {
-        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
-        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
-        // "Task_1.Models.Model1" в экземпляре LocalDb. 
-        // 
-        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
-        // в файле конфигурации приложения.
-        public Model1()
-            : base("name=Model1")
-        {
-        }
-        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
-        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
-        
-        public class Calculating
-        {
-            public object X { get; set; }
-            public object Y { get; set; }
-            public int Result { get; set; }
-                      
-        }
-    }
-
     public static class Expansion
     {
         public static List<string> log = new List<string>();
@@ -42,7 +17,6 @@
         /// <returns>Возвращает массив вычислений</returns>
         public static List<string> GetLog(this HtmlHelper html)
         {
-            List<string> log = new List<string>();
             return log;
         }
     }
